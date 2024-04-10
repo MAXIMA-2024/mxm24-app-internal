1e7db0dde5038f63421dedf2fda2cdaa2b2d472a
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-04-10 13:33:42 +0700

    feat: add react-icons, sidebar desktop 70%

--- src/pages/dashboard/_layout.tsx
+++ src/pages/dashboard/_layout.tsx
@@ -1,5 +1,7 @@
 import { Stack, Text, Heading, Image, Button, Divider } from "@chakra-ui/react";
-import { Outlet, useLocation } from "react-router-dom";
+import { Outlet, useLocation, Link } from "react-router-dom";
+import { HiChevronDown } from "react-icons/hi";
+import { MdAccountCircle } from "react-icons/md";
 
 const DesktopLayout = () => {};
 
@@ -22,41 +24,171 @@ const DashboardLayout = () => {
         alignItems={"center"}
         justifyContent={"space-between"}
         shadow={"lg"}
+        pb={8}
       >
         {/* Sidebar */}
         <Stack w={"full"}>
-          <Stack direction={"row"}>
-            <Image src="/logoMaxima.svg" w={"2rem"}></Image>
-            <Heading
-              fontFamily={"Poppins"}
-              fontSize={"1.5rem"}
-              fontWeight={"semibold"}
+          <Link to="/dashboard">
+            <Stack direction={"row"}>
+              <Image src="logo.png" w={"2rem"} mr={"1rem"}></Image>
+              <Heading
+                fontFamily={"Poppins"}
+                fontSize={"1.5rem"}
+                fontWeight={"semibold"}
+              >
+                MAXIMA 2024
+              </Heading>
+            </Stack>
+          </Link>
+          <Stack w={"full"} mt={"2rem"} gap={"0.75rem"}>
+            <Link to="/dashboard/verification">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"start"}
+                p={5}
+              >
+                <Image
+                  src="icons/verification.png"
+                  w={"1rem"}
+                  mr={"1rem"}
+                ></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  Verifikasi
+                </Text>
+              </Button>
+            </Link>
+            <Link to="/dashboard/toggles">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"start"}
+                p={5}
+              >
+                <Image src="icons/toggle.png" w={"1rem"} mr={"1rem"}></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  Toggles
+                </Text>
+              </Button>
+            </Link>
+            <Divider
+              bgColor={"#EFEFEF"}
+              height={"0.1rem"}
+              mx={"-10"}
+              w={"325px"}
+            />
+            <Link to="/dashboard">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"start"}
+                p={5}
+              >
+                <Image src="icons/dashboard.png" w={"1rem"} mr={"1rem"}></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  Dashboard
+                </Text>
+              </Button>
+            </Link>
+            <Link to="/dashboard/panitia">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"start"}
+                p={5}
+              >
+                <Image src="icons/panitia.png" w={"1rem"} mr={"1rem"}></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  Panitia
+                </Text>
+              </Button>
+            </Link>
+            <Link to="/dashboard/organisator">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"start"}
+                p={5}
+              >
+                <Image
+                  src="icons/organisator.png"
+                  w={"1rem"}
+                  mr={"1rem"}
+                ></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  Organisator
+                </Text>
+              </Button>
+            </Link>
+            <Link to="/dashboard/mahasiswa">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"start"}
+                p={5}
+              >
+                <Image src="icons/mahasiswa.png" w={"1rem"} mr={"1rem"}></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  Mahasiswa
+                </Text>
+              </Button>
+            </Link>
+            <Button
+              variant={"ghost"}
+              w={"full"}
+              justifyContent={"space-between"}
+              p={5}
             >
-              MAXIMA 2024
-            </Heading>
-          </Stack>
-          <Stack w={"full"} mt={"2rem"}>
-            <Button variant={"ghost"} w={"full"} justifyContent={"start"} p={0}>
-              <Image src="/logoMaxima.svg" w={"1rem"} mr={"1rem"}></Image>
-              <Text fontSize={"1rem"}>Verifikasi</Text>
+              <Stack direction={"row"}>
+                <Image
+                  src="icons/state.png"
+                  w={"1.5rem"}
+                  mr={"0.75rem"}
+                ></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  State
+                </Text>
+              </Stack>
+              <HiChevronDown />
             </Button>
-            <Button variant={"ghost"} w={"full"} justifyContent={"start"} p={0}>
-              <Image src="/logoMaxima.svg" w={"1rem"} mr={"1rem"}></Image>
-              <Text fontSize={"1rem"}>Toggles</Text>
-            </Button>
-            <Divider />
-            <Button variant={"ghost"} w={"full"} justifyContent={"start"} p={0}>
-              <Image src="/logoMaxima.svg" w={"1rem"} mr={"1rem"}></Image>
-              <Text fontSize={"1rem"}>Dashboard</Text>
-            </Button>
-            <Button variant={"ghost"} w={"full"} justifyContent={"start"} p={0}>
-              <Image src="/logoMaxima.svg" w={"1rem"} mr={"1rem"}></Image>
-              <Text fontSize={"1rem"}>Tables</Text>
-            </Button>
+            <Link to="/malpun">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"start"}
+                p={5}
+              >
+                <Image src="icons/malpun.png" w={"1rem"} mr={"1rem"}></Image>
+                <Text fontSize={"1rem"} fontWeight={"medium"}>
+                  Malpun
+                </Text>
+              </Button>
+            </Link>
+            <Link to="/qrscanner">
+              <Button
+                variant={"ghost"}
+                w={"full"}
+                justifyContent={"space-between"}
+                p={5}
+              >
+                <Stack direction={"row"}>
+                  <Image src="icons/qr.png" w={"1.5rem"} mr={"0.75rem"}></Image>
+                  <Text fontSize={"1rem"} fontWeight={"medium"}>
+                    QR Scan
+                  </Text>
+                </Stack>
+                <HiChevronDown />
+              </Button>
+            </Link>
           </Stack>
         </Stack>
         <Stack w={"full"}>
-          <Divider />
+          <Divider
+            bgColor={"#EFEFEF"}
+            height={"0.1rem"}
+            mx={"-10"}
+            w={"325px"}
+          />
           <Button
             variant={"ghost"}
             w={"full"}
@@ -64,17 +196,19 @@ const DashboardLayout = () => {
             p={0}
             mt={"1rem"}
           >
-            <Image src="/logoMaxima.svg" w={"2rem"} mr={"1.5rem"}></Image>
+            <MdAccountCircle size={"3rem"} opacity={"0.75"} />
             <Stack
               justifyContent={"center"}
               alignItems={"start"}
+              ml={"0.5rem"}
               gap={0}
-              mb={"0.25rem"}
             >
-              <Text fontSize={"1.25rem"} fontWeight={"bold"}>
-                John Doe
+              <Text fontSize={"1rem"} fontWeight={"medium"}>
+                John Ryan
               </Text>
-              <Text fontSize={"0.75rem"}>Admin</Text>
+              <Text fontSize={"0.75rem"} fontWeight={"medium"} opacity={"0.75"}>
+                Charta
+              </Text>
             </Stack>
           </Button>
         </Stack>
