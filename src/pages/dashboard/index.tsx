79c7dd883cb0b8a5fd517148be0ae4fe99354a35
rdhwan <fathanridhwann@gmail.com>
2024-04-12 23:29:25 +0700

    fix: change example index into flex

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,26 +1,19 @@
-import { Heading, Text, Box, Stack, Show } from "@chakra-ui/react";
+import { Heading, Text, Stack, Show } from "@chakra-ui/react";
 
 const Dashboard = () => {
   return (
     <>
-      <Stack w={"full"} h={"full"}>
+      <Stack flex={1}>
         <Show above={"md"}>
           <Heading fontFamily={"Poppins"} color={"text.primary"}>
             Dashboard
           </Heading>
         </Show>
-        <Box
-          bgColor={"white"}
-          w={"full"}
-          h={"full"}
-          shadow={"lg"}
-          p={25}
-          rounded={"xl"}
-        >
+        <Stack bgColor={"white"} flex={1} shadow={"lg"} p={25} rounded={"xl"}>
           <Text fontWeight={"medium"} color={"text.primary"} opacity={0.8}>
             Selamat datang, <strong>John Ryan R.</strong> 🤩
           </Text>
-        </Box>
+        </Stack>
       </Stack>
     </>
   );
