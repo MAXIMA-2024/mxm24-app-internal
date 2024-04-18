87e003c1d2996d191224e01f4bf1e6e137aff3c3
owin40 <godwinwoisiri40@gmail.com>
2024-04-18 13:41:35 +0700

    feat: layout daftar state & detail dan peserta

--- src/pages/dashboard/state/index.tsx
+++ src/pages/dashboard/state/index.tsx
@@ -7,87 +7,177 @@ import {
   Stack,
   Tag,
   Show,
+  Text,
+  Divider,
+  // Modal,
 } from "@chakra-ui/react";
+import { MdDeleteForever, MdInfo } from "react-icons/md";
 import { Link } from "react-router-dom";
 import DataTable from "../../../components/datatables";
 import { MUIDataTableColumn } from "mui-datatables";
+import { useEffect, useState } from "react";
+import { Button as MuiButton } from "@mui/material";
+// import useAuth from "@/hooks/useAuth";
 
-const STATE = () => {
+const isSuperadmin = (user: { role: string }) => {
+  return user.role === "superadmin";
+};
+
+const isPanitia = (user: { role: string }) => {
+  return user.role === "panitia";
+};
+
+const isOrganisator = (user: { role: string }) => {
+  return user.role === "organisator";
+};
+
+const Organisator = () => {
+  // const { user } = useAuth();
+
+  // mock data dummy
+  // const mockUser = { role: "superadmin" };
+  // const mockUser = { role: "panitia" };
+  const mockUser = { role: "organisator" };
+  const user = mockUser;
+
+  const [role, setRole] = useState<
+    "superadmin" | "panitia" | "organisator" | null
+  >(null);
+
+  useEffect(() => {
+    if (user) {
+      if (isSuperadmin(user)) {
+        setRole("superadmin");
+      } else if (isOrganisator(user)) {
+        setRole("organisator");
+      } else if (isPanitia(user)) {
+        setRole("panitia");
+      }
+    }
+  }, [user]);
+
+  type ModalState = {
+    id?: number;
+    mode: "create" | "delete"; // enum
+  };
+
+  const [modalState, setModalState] = useState<ModalState | undefined>();
+
+  let actionColumn: MUIDataTableColumn = {
+    name: "",
+  }; // init empty object
+
+  if (role === "superadmin" || role === "panitia") {
+    actionColumn = {
+      name: "id",
+      label: "Action",
+      options: {
+        customBodyRender: (value: number) => {
+          return (
+            <Stack direction={"row"} gap={"1rem"}>
+              {(role === "superadmin" || role === "panitia") && (
+                <>
+                  <Link to={`/details/${value}`}>
+                    <MuiButton
+                      variant={"contained"}
+                      color={"primary"}
+                      sx={{ borderRadius: "4rem" }}
+                    >
+                      <MdInfo />
+                      Detail
+                    </MuiButton>
+                  </Link>
+                  <MuiButton
+                    variant={"contained"}
+                    color={"error"}
+                    sx={{
+                      borderRadius: "md",
+                      minWidth: "0",
+                      paddingX: "0.5rem",
+                      boxShadow: "none",
+                    }}
+                    onClick={() => setModalState({ id: value, mode: "delete" })}
+                  >
+                    <MdDeleteForever />
+                  </MuiButton>
+                </>
+              )}
+            </Stack>
+          );
+        },
+      },
+    };
+  }
+
   const colDefs: MUIDataTableColumn[] = [
     {
-      name: "name",
-      label: "Name",
+      name: "namastate",
+      label: "Nama STATE",
     },
     {
-      name: "nim",
-      label: "NIM",
+      name: "kuota",
+      label: "Kuota",
     },
-    {
-      name: "status",
-      label: "Panitia/Organisator",
-    },
-    {
-      name: "divisi",
-      label: "Divisi/Kategori",
-    },
+    actionColumn,
   ];
 
   const data = [
-    ["Joe James", "Test Corp", "Yonkers", "NY"],
-    ["John Walsh", "Test Corp", "Hartford", "CT"],
-    ["Bob Herm", "Test Corp", "Tampa", "FL"],
-    ["James Houston", "Test Corp", "Dallas", "TX"],
-    ["Joe James", "Test Corp", "Yonkers", "NY"],
-    ["John Walsh", "Test Corp", "Hartford", "CT"],
-    ["Bob Herm", "Test Corp", "Tampa", "FL"],
-    ["James Houston", "Test Corp", "Dallas", "TX"],
-    ["Joe James", "Test Corp", "Yonkers", "NY"],
-    ["John Walsh", "Test Corp", "Hartford", "CT"],
-    ["Bob Herm", "Test Corp", "Tampa", "FL"],
-    ["James Houston", "Test Corp", "Dallas", "TX"],
+    ["Joe James", "12/345"],
+    ["John Walsh", "12/346"],
+    ["Bob Herm", "12/347"],
+    ["James Houston", "12/348"],
+    ["Joe James", "12/349"],
+    ["John Walsh", "12/350"],
+    ["Bob Herm", "12/351"],
+    ["James Houston", "12/352"],
+    ["Joe James", "12/353"],
+    ["John Walsh", "12/354"],
+    ["Bob Herm", "12/355"],
+    ["James Houston", "12/356"],
   ];
 
   return (
     <>
       <style>
         {`
-          .tss-hwdp7s-MUIDataTable-liveAnnounce {
-              border: 0 !important;
-              clip: rect(0 0 0 0) !important;
-              height: 1px !important;
-              margin: -1px !important;
-              overflow: hidden !important;
-              padding: 0 !important;
-              position: relative !important;
-              width: 1px !important;
-          }
-        `}
+            .tss-hwdp7s-MUIDataTable-liveAnnounce {
+                border: 0 !important;
+                clip: rect(0 0 0 0) !important;
+                height: 1px !important;
+                margin: -1px !important;
+                overflow: hidden !important;
+                padding: 0 !important;
+                position: relative !important;
+                width: 1px !important;
+            }
+          `}
       </style>
 
-      <Stack gap={7}>
-        {/* Breadcrumb */}
-        <Show above="md">
-          <Breadcrumb fontWeight="medium" fontSize="sm">
-            <BreadcrumbItem>
-              <Link to={"/dashboard"}>
-                <BreadcrumbLink>Dashboard</BreadcrumbLink>
-              </Link>
-            </BreadcrumbItem>
-            <BreadcrumbItem isCurrentPage>
-              <Link to={"/dashboard/state"}>
-                <BreadcrumbLink color={"brand.maroon"} fontWeight={"medium"}>
-                  STATE
-                </BreadcrumbLink>
-              </Link>
-            </BreadcrumbItem>
-          </Breadcrumb>
+      {/* page superadmin */}
+      {(role === "superadmin" || role === "panitia") && (
+        <Stack gap={7}>
+          {/* Breadcrumb */}
+          <Show above="md">
+            <Breadcrumb fontWeight="medium" fontSize="sm">
+              <BreadcrumbItem>
+                <Link to={"/dashboard"}>
+                  <BreadcrumbLink>Dashboard</BreadcrumbLink>
+                </Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem isCurrentPage>
+                <Link to={"/dashboard/state/daftarstate"}>
+                  <BreadcrumbLink color={"brand.maroon"} fontWeight={"medium"}>
+                    Daftar STATE
+                  </BreadcrumbLink>
+                </Link>
+              </BreadcrumbItem>
+            </Breadcrumb>
 
-          {/* Header */}
-          <Stack direction={"row"} gap={5}>
-            <Heading fontFamily={"Poppins"} color={"text.primary"}>
-              STATE
-            </Heading>
-            <Stack justifyContent={"end"} mb={2}>
+            {/* Header */}
+            <Stack direction={"row"} gap={5}>
+              <Heading fontFamily={"Poppins"} color={"text.primary"}>
+                Daftar STATE
+              </Heading>
               <Tag
                 bgColor={"brand.maroon"}
                 h={25}
@@ -98,22 +188,92 @@ const STATE = () => {
                 Superadmin
               </Tag>
             </Stack>
-          </Stack>
-        </Show>
-        {/* Content */}
-        <Box
-          bgColor={"white"}
-          w={"full"}
-          // h={"full"}
-          shadow={"lg"}
-          rounded={"xl"}
-          overflow={"auto"}
-        >
-          {data && <DataTable colDefs={colDefs} data={data} />}
-        </Box>
-      </Stack>
+          </Show>
+          {/* Content */}
+          <Box
+            bgColor={"white"}
+            w={"full"}
+            // h={"full"}
+            shadow={"lg"}
+            rounded={"xl"}
+            overflow={"auto"}
+          >
+            {data && <DataTable colDefs={colDefs} data={data} />}
+          </Box>
+        </Stack>
+      )}
+
+      {/* page organisator biasa */}
+      {role === "organisator" && (
+        <Stack gap={7}>
+          {/* Breadcrumb */}
+          <Show above="md">
+            <Breadcrumb fontWeight="medium" fontSize="sm">
+              <BreadcrumbItem>
+                <Link to={"/dashboard"}>
+                  <BreadcrumbLink>Dashboard</BreadcrumbLink>
+                </Link>
+              </BreadcrumbItem>
+              <BreadcrumbItem isCurrentPage>
+                <Link to={"/dashboard/state/daftarstate"}>
+                  <BreadcrumbLink color={"brand.maroon"} fontWeight={"medium"}>
+                    Detail dan Peserta
+                  </BreadcrumbLink>
+                </Link>
+              </BreadcrumbItem>
+            </Breadcrumb>
+
+            {/* Header */}
+            <Stack direction={"row"} gap={5}>
+              <Heading fontFamily={"Poppins"} color={"text.primary"}>
+                Detail dan Peserta
+              </Heading>
+            </Stack>
+          </Show>
+          {/* Content */}
+          <Box
+            bgColor={"white"}
+            w={"full"}
+            // h={"full"}
+            shadow={"lg"}
+            rounded={"xl"}
+            overflow={"auto"}
+          >
+            <Stack
+              mt={7}
+              direction="row"
+              divider={<Divider orientation="vertical" />}
+              spacing={2}
+              gap={10}
+              justifyContent="center"
+            >
+              <Stack maxW="379px">
+                <img src="" alt="placeholderimage" />
+              </Stack>
+              <Stack maxW="379px">
+                <Heading size="lg">UKM 1</Heading>
+                <Text>Hari ke-1 (Kamis, 20 Agustus 2023)</Text>
+                <Text>40/100</Text>
+                <Text>Lecture Hall</Text>
+              </Stack>
+              <Stack maxW="379px">
+                <Heading size="md">Deskripsi</Heading>
+                <Text fontSize="sm">
+                  Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam
+                  rutrum mauris viverra ligula pulvinar blandit. Curabitur
+                  molestie ante pretium lorem lacinia, a lacinia risus volutpat.
+                  Fusce cursus pharetra quam ac rutrum. Nullam a velit non dui
+                  ullamcorper ultricies ac id velit. Suspendisse ex donec.
+                </Text>
+              </Stack>
+            </Stack>
+
+            {data && <DataTable colDefs={colDefs} data={data} />}
+          </Box>
+        </Stack>
+      )}
     </>
   );
 };
 
-export default STATE;
+export default Organisator;
