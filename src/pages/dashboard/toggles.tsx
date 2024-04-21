48ef7fe68c3bc8f4adcb10f0e7552a9b3a68ff22
AlbertTech23 <alberttirtokusumo@gmail.com>
2024-04-21 10:50:39 +0700

    feat: toggles add button + modal done

--- src/pages/dashboard/toggles.tsx
+++ src/pages/dashboard/toggles.tsx
@@ -7,18 +7,75 @@ import {
   Stack,
   Tag,
   Show,
+  Modal,
+  ModalOverlay,
+  ModalContent,
+  ModalHeader,
+  ModalFooter,
+  ModalBody,
+  ModalCloseButton,
+  Button,
+  useDisclosure,
+  useToast,
+  FormControl,
+  FormErrorMessage,
+  FormLabel,
+  Input,
 } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 import DataTable from "../../components/datatables";
 import { MUIDataTableColumn } from "mui-datatables";
 import Switch from "@mui/material/Switch";
 import { useState } from "react";
-
+import React from "react";
+import { z } from "zod";
+// import { useToastErrorHandler } from "@/hooks/useApi";
+import { useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod";
 interface Toggle {
   id: string;
   status: boolean;
 }
+
+type Toggles = {
+  id: number;
+  nama_kegiatan: string;
+  status: boolean;
+};
+
+type TogglesFillable = Pick<Toggles, "nama_kegiatan">;
+
+const togglesSchema = z.object({
+  nama_kegiatan: z
+    .string({ required_error: "Nama kegiatan harus diisi" })
+    .min(1, "Nama kegiatan harus diisi")
+    .max(255, "Nama barang maksimal 255 karakter"),
+  // .min(5, "Nama barang minimal 5 karakter"),
+});
+
 const Toggles = () => {
+  // const errorHandler = useToastErrorHandler();
+  const toast = useToast();
+
+  const {
+    handleSubmit,
+    register,
+    formState: { errors },
+  } = useForm<TogglesFillable>({
+    resolver: zodResolver(togglesSchema),
+  });
+
+  // toggles useState
+  // const [modalToggles, setModalToggles] = useState<ModalToggles | undefined>();
+
+  // Modal Blur Overlay
+  const OverlayOne = () => (
+    <ModalOverlay bg="blackAlpha.300" backdropFilter="blur(10px)" />
+  );
+
+  const { isOpen, onOpen, onClose } = useDisclosure();
+  const [overlay, setOverlay] = React.useState(<OverlayOne />);
+
   const [toggleData, setToggleData] = useState<Toggle[]>([
     { id: "Register", status: true },
     { id: "HoME", status: false },
@@ -95,21 +152,32 @@ const Toggles = () => {
           </Breadcrumb>
 
           {/* Header */}
-          <Stack direction={"row"} gap={5}>
-            <Heading fontFamily={"Poppins"} color={"text.primary"}>
-              Toggles
-            </Heading>
-            <Stack justifyContent={"end"} mb={2}>
-              <Tag
-                bgColor={"brand.maroon"}
-                h={25}
-                color={"white"}
-                rounded={"full"}
-                fontSize={"0.75rem"}
-              >
-                Superadmin
-              </Tag>
+          <Stack direction={"row"} justifyContent={"space-between"} gap={5}>
+            <Stack gap={5} direction={"row"}>
+              <Heading fontFamily={"Poppins"} color={"text.primary"}>
+                Toggles
+              </Heading>
+              <Stack justifyContent={"end"} mb={2}>
+                <Tag
+                  bgColor={"brand.maroon"}
+                  h={25}
+                  color={"white"}
+                  rounded={"full"}
+                  fontSize={"0.75rem"}
+                >
+                  Superadmin
+                </Tag>
+              </Stack>
             </Stack>
+            <Button
+              onClick={() => {
+                setOverlay(<OverlayOne />);
+                onOpen();
+              }}
+              colorScheme="blue"
+            >
+              + Add Toggles
+            </Button>
           </Stack>
         </Show>
         {/* Content */}
@@ -124,6 +192,108 @@ const Toggles = () => {
           {toggleData && <DataTable colDefs={colDefs} data={toggleData} />}
         </Box>
       </Stack>
+      <Modal isCentered isOpen={isOpen} onClose={onClose}>
+        {overlay}
+        <ModalContent>
+          <ModalHeader>Add Toggles</ModalHeader>
+          <ModalCloseButton />
+          <ModalBody>
+            <form
+              id="add-toggles"
+              onSubmit={handleSubmit((data) => {
+                if (Object.keys(errors).length === 0) {
+                  console.log(data);
+                  toast({
+                    title: "Created",
+                    description: `Kegiatan ${data.nama_kegiatan} has been created`,
+                    status: "success",
+                  });
+                  onClose();
+                  // API call logic here
+                } else {
+                  console.error("Form errors:", errors);
+                }
+                // api
+                //   .post("/barang", data)
+                //   .then(() => {
+                //     toast({
+                //       title: "Created",
+                //       description: `Barang ${data.kode_sku} has been created`,
+                //       status: "success",
+                //     });
+                //   })
+                //   .catch(errorHandler)
+                //   .finally(() => {
+                //     mutate();
+                //     setModalState(undefined);
+                //   });
+              })}
+            >
+              {/* kode_sku */}
+              {/* <FormControl isInvalid={!!errors.kode_sku}>
+                <FormLabel>Kode SKU</FormLabel>
+                <Input
+                  placeholder="Kode SKU"
+                  {...register("kode_sku")}
+                  type="text"
+                />
+                <FormErrorMessage>
+                  {errors.kode_sku && errors.kode_sku.message}
+                </FormErrorMessage>
+              </FormControl> */}
+
+              {/* nama_kegiatan */}
+              <FormControl isInvalid={!!errors.nama_kegiatan}>
+                <FormLabel>Nama Kegiatan</FormLabel>
+                <Input
+                  placeholder="Nama Kegiatan"
+                  {...register("nama_kegiatan")}
+                  type="text"
+                />
+                <FormErrorMessage>
+                  {errors.nama_kegiatan && errors.nama_kegiatan.message}
+                </FormErrorMessage>
+              </FormControl>
+
+              {/* qty */}
+              {/* <FormControl isInvalid={!!errors.qty}>
+                <FormLabel>Quantity</FormLabel>
+                <Input
+                  placeholder="Quantity"
+                  {...register("qty", {
+                    valueAsNumber: true,
+                  })}
+                  type="number"
+                />
+                <FormErrorMessage>
+                  {errors.qty && errors.qty.message}
+                </FormErrorMessage>
+              </FormControl> */}
+
+              {/* harga */}
+              {/* <FormControl isInvalid={!!errors.harga}>
+                <FormLabel>Harga</FormLabel>
+                <Input
+                  placeholder="Harga (dalam Rupiah)"
+                  {...register("harga", {
+                    valueAsNumber: true,
+                  })}
+                  type="number"
+                />
+                <FormErrorMessage>
+                  {errors.harga && errors.harga.message}
+                </FormErrorMessage>
+              </FormControl> */}
+            </form>
+          </ModalBody>
+          <ModalFooter>
+            {/* <Button onClick={onClose}>Close</Button> */}
+            <Button colorScheme="blue" type="submit" form="add-toggles">
+              Add
+            </Button>
+          </ModalFooter>
+        </ModalContent>
+      </Modal>
     </>
   );
 };
