6a2388fce3e30eb616df14f6ea88d6a9cd0548e1
Gian Avila <avila.gian111@gmail.com>
2024-04-21 13:38:46 +0700

    feat: responsive layout for mobile size and slider for cardData

--- src/pages/dashboard/index.tsx
+++ src/pages/dashboard/index.tsx
@@ -1,150 +1,153 @@
-import { Heading, Text, Stack, Show, Image} from "@chakra-ui/react";
-import iconPanitia from "../../../public/icons/iconPanitia.png";
-import iconMahasiswa from "../../../public/icons/iconMahasiswa.png";
-import iconSTATE from "../../../public/icons/iconSTATE.png";
-import iconMalPun from "../../../public/icons/iconMalPun.png";
+import { Heading, Text, Stack, Show, Image, Hide } from "@chakra-ui/react";
+import iconPanitia from "/icons/iconPanitia.png";
+import iconMahasiswa from "/icons/iconMahasiswa.png";
+import iconSTATE from "/icons/iconSTATE.png";
+import iconMalPun from "/icons/iconMalPun.png";
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+
 const Dashboard = () => {
+  const cardsData = [
+    { nama: "Panitia", icon: iconPanitia, angka: 249, bgColor: "orange" },
+    {
+      nama: "Mahasiswa",
+      icon: iconMahasiswa,
+      angka: 49,
+      bgColor: "yellow.200",
+    },
+    { nama: "STATE", icon: iconSTATE, angka: 3409, bgColor: "orange.300" },
+    { nama: "MalPun", icon: iconMalPun, angka: 245, bgColor: "orange.100" },
+  ];
+
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplaySpeed: 2000,
+  };
+
   return (
     <>
-      <Stack flex={1}>
-        <Show above={"mt"}>
-          <Heading fontFamily={"Poppins"} color={"text.primary"} >
+      <style>
+        {`.slider{
+          // margin: 10px;
+
+        }
+        `}
+      </style>
+      <Stack flex={1} spacing={6}>
+        <Show above={"md"}>
+          <Heading fontFamily={"Poppins"} color={"text.primary"}>
             Dashboard
           </Heading>
         </Show>
-        <Stack bgColor={"white"} flex={1} shadow={"lg"} p={25} rounded={"xl"}>
-          <Text fontWeight={"medium"} color={"text.primary"} opacity={0.8} >
+
+        <Stack
+          bgColor={"white"}
+          flex={1}
+          shadow={"lg"}
+          p={25}
+          rounded={"xl"}
+          w={["full", "full", "29rem", "40rem", "full"]}
+        >
+          <Text fontWeight={"medium"} color={"text.primary"} opacity={0.8}>
             Selamat datang, <strong>John Ryan R.</strong> 🤩
           </Text>
+
           {/* TAB CARDS START */}
-          <Text fontWeight={"medium"} color={"text.primary"} opacity={0.8} m={1} marginTop={5}>
+          <Text
+            fontWeight={"medium"}
+            color={"text.primary"}
+            opacity={0.8}
+            m={1}
+            marginTop={5}
+          >
             Tab
           </Text>
-          <Stack direction={"row"} spacing={2}>
-            {/* CARD 1 START */}
-            <Stack direction={"column"} spacing={6}>
-              <Stack
-                bgColor="orange"
-                w={"200px"}
-                h={"120px"}
-                borderRadius={15}
-                p={4}
-                flexDirection={"column"}
-                spacing={3}
-              >
-                <Stack
-                  direction={"row"}
-                  spacing={2}
-                  justifyContent={"space-between"}
-                >
-                  <Text fontWeight={"medium"} fontSize={"xl"}>
-                    Panitia
-                  </Text>
-                  
-                  <Image src={iconPanitia}></Image>
-                  {/* Fixed the alt prop */}
-                  
+
+          <Hide above={"md"}>
+            <Slider {...settings}>
+              {cardsData.map((data, index) => (
+                <Stack key={index} px={2}>
+                  <Stack
+                    bgColor={data.bgColor}
+                    w={["full", "full", "225px"]}
+                    h={"120px"}
+                    borderRadius={15}
+                    p={4}
+                    flexDirection={"column"}
+                    spacing={3}
+                  >
+                    <Stack
+                      direction={"row"}
+                      spacing={2}
+                      justifyContent={"space-between"}
+                    >
+                      <Text fontWeight={"medium"} fontSize={"xl"}>
+                        {data.nama}
+                      </Text>
+                      <Image src={data.icon} />
+                    </Stack>
+                    <Text fontWeight={"bold"} fontSize={"3xl"}>
+                      {data.angka}
+                    </Text>
+                  </Stack>
                 </Stack>
+              ))}
+            </Slider>
+          </Hide>
 
-                <Text fontWeight={"bold"} fontSize={"3xl"}>
-                  249
-                </Text>
-              </Stack>
-            </Stack>{" "}
-            {/* CARD 1 END */}
-            {/* CARD 2 START */}
-            <Stack direction={"row"} spacing={6}>
-              <Stack
-                bgColor="yellow.200"
-                w={"200px"}
-                h={"120px"}
-                borderRadius={15}
-                p={4}
-                flexDirection={"column"}
-                spacing={3}
-              >
-                <Stack
-                  direction={"row"}
-                  spacing={2}
-                  justifyContent={"space-between"}
-                >
-                  <Text fontWeight={"medium"} fontSize={"xl"}>
-                    Mahasiswa
-                  </Text>
-
-                  <Image src={iconMahasiswa}></Image>
+          <Show above="md">
+            <Stack direction={"row"} spacing={6} overflow={"auto"}>
+              {cardsData.map((data, index) => (
+                <Stack key={index} direction={"column"} spacing={6}>
+                  <Stack
+                    bgColor={data.bgColor}
+                    w={["275px", "350px", "225px", "225px"]}
+                    h={"120px"}
+                    borderRadius={15}
+                    p={4}
+                    flexDirection={"column"}
+                    spacing={3}
+                  >
+                    <Stack
+                      direction={"row"}
+                      spacing={2}
+                      justifyContent={"space-between"}
+                    >
+                      <Text fontWeight={"medium"} fontSize={"xl"}>
+                        {data.nama}
+                      </Text>
+                      <Image src={data.icon} />
+                    </Stack>
+                    <Text fontWeight={"bold"} fontSize={"3xl"}>
+                      {data.angka}
+                    </Text>
+                  </Stack>
                 </Stack>
-
-                <Text fontWeight={"bold"} fontSize={"3xl"}>
-                  49
-                </Text>
-              </Stack>
-            </Stack>{" "}
-            {/* CARD 2 END */}
-            {/* CARD 3 START */}
-            <Stack direction={"row"} spacing={6}>
-              <Stack
-                bgColor="orange.300"
-                w={"200px"}
-                h={"120px"}
-                borderRadius={15}
-                p={4}
-                flexDirection={"column"}
-                spacing={3}
-              >
-                <Stack
-                  direction={"row"}
-                  spacing={2}
-                  justifyContent={"space-between"}
-                >
-                  <Text fontWeight={"medium"} fontSize={"xl"}>
-                    STATE
-                  </Text>
-
-                  <Image src={iconSTATE}></Image>
-                </Stack>
-
-                <Text fontWeight={"bold"} fontSize={"3xl"}>
-                  3409
-                </Text>
-              </Stack>
-            </Stack>{" "}
-            {/* CARD 3 END */}
-            {/* CARD 4 START */}
-            <Stack direction={"row"} spacing={6}>
-              <Stack
-                bgColor="orange.100"
-                w={"200px"}
-                h={"120px"}
-                borderRadius={15}
-                p={4}
-                flexDirection={"column"}
-                spacing={3}
-              >
-                <Stack
-                  direction={"row"}
-                  spacing={2}
-                  justifyContent={"space-between"}
-                >
-                  <Text fontWeight={"medium"} fontSize={"xl"}>
-                    MalPun
-                  </Text>
-
-                  <Image src={iconMalPun}></Image>
-                </Stack>
-
-                <Text fontWeight={"bold"} fontSize={"3xl"}>
-                  245
-                </Text>
-              </Stack>
-            </Stack>{" "}
-            {/* CARD 4 END */}
-          </Stack>
+              ))}
+            </Stack>
+          </Show>
           {/* TAB CARDS END */}
-          <Text fontWeight={"medium"} color={"text.primary"} opacity={0.8} m={1} marginTop={5}>
+
+          <Text
+            fontWeight={"medium"}
+            color={"text.primary"}
+            opacity={0.8}
+            m={1}
+            marginTop={5}
+          >
             Grafik
           </Text>
-          <Text fontWeight={"medium"} color={"text.primary"} opacity={0.8} m={6}>
+          <Text
+            fontWeight={"medium"}
+            color={"text.primary"}
+            opacity={0.8}
+            m={6}
+          >
             <strong>Mahasiswa / Peserta STATE</strong>
           </Text>
         </Stack>
